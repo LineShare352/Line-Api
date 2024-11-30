e308cd37864511eeaa64a2cf9a6da50a39f5f756
Line <wiraliwirya@gmail.com>
2024-11-30 08:14:55 +0700

    Add files via upload

---
+++ api/random/korea.js
@@ -0,0 +1,30 @@
+const cors = require('cors');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.enable("trust proxy");
+app.use(cors());
+
+app.get('/api/random/korea', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.siputzx.my.id/api/r/cecan/korea?-', {
+      responseType: 'stream', 
+    });
+
+    res.setHeader('Content-Type', response.headers['content-type']);
+
+    response.data.pipe(res);
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(500).json({
+      status: false,
+      message: "Failed to fetch image from the source.",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
