57468614e673e5f2dd820d7d9744248412b8387a
Line <wiraliwirya@gmail.com>
2024-11-30 07:45:58 +0700

    Add files via upload

---
+++ api/games/tebakkabupaten.js
@@ -0,0 +1,36 @@
+const cors = require('cors');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.enable("trust proxy");
+app.set("json spaces", 2);
+app.use(cors());
+
+app.get('/api/games/tebakkabupaten', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.siputzx.my.id/api/games/kabupaten?-');
+
+    return res.status(200).json({
+      status: true,
+      creator: "Hello Line",
+      data: {
+        link: response.data.data.link,
+        title: response.data.data.title, 
+        url: response.data.data.url
+      }
+    });
+  } catch (e) {
+    console.error("Error:", e.message);
+    return res.status(500).json({
+      status: false,
+      creator: "Hello Line",
+      error: "Internal server error.",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
