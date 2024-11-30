dfd8272a5eb57f1e34e17f47dae48e2f99f5b7e8
Line <wiraliwirya@gmail.com>
2024-11-30 07:48:05 +0700

    Create tiktok.js

---
+++ api/stalk/tiktok.js
@@ -0,0 +1,42 @@
+const cors = require('cors');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.enable("trust proxy");
+app.set("json spaces", 2);
+app.use(cors());
+
+app.get('/api/stalk/tiktok', async (req, res) => {
+  try {
+    const { text } = req.query; 
+
+    if (!text) {
+      return res.status(400).json({
+        status: false,
+        creator: "Hello Line",
+        error: "Query parameter 'username' is required."
+      });
+    }
+
+    const response = await axios.get(`https://api.siputzx.my.id/api/stalk/tiktok?username=${encodeURIComponent(text)}`);
+
+    return res.status(200).json({
+      status: true,
+      creator: "Hello Line",
+      data: response.data.data 
+    });
+  } catch (e) {
+    console.error("Error:", e.message);
+    return res.status(500).json({
+      status: false,
+      creator: "Hello Line",
+      error: "Internal server error.",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
