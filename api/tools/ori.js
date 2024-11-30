c12a17768f9b22764fb70912a626846e462f8db6
Line <wiraliwirya@gmail.com>
2024-11-30 16:19:16 +0700

    Add files via upload

--- api/tools/ori.js
+++ api/tools/ori.js
@@ -7,30 +7,37 @@ app.enable("trust proxy");
 app.set("json spaces", 2);
 app.use(cors());
 
+function toGhOri(rawUrl) {
+  return rawUrl
+    .replace('raw.githubusercontent.com', 'github.com')
+    .replace('/master', '/blob/master')
+    .replace('/main', '/blob/main');
+}
+
 app.get('/api/tools/ori', async (req, res) => {
   const { url } = req.query;
 
   if (!url || !url.includes('raw.githubusercontent.com')) {
     return res.status(400).json({
       status: false,
-      creator: 'Hello Line', 
-      result: 'Invalid or missing GitHub URL.',
+      creator: 'Hello Line',
+      result: 'Invalid or missing GitHub Raw URL.',
     });
   }
 
   try {
-    const oriUrl = url.replace('raw.githubusercontent.com', 'github.com').replace('/blob/', '/');
+    const originalUrl = toGhOri(url);
     return res.json({
       status: true,
       creator: 'Hello Line',
-      result: `${oriUrl}`,
-    }); 
+      result: originalUrl,
+    });
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({
       status: false,
-      creator: 'Hello Line', 
-      result: 'Failed to convert GitHub URL to original.',
+      creator: 'Hello Line',
+      result: 'Failed to convert GitHub Raw URL to original.',
     });
   }
 });
